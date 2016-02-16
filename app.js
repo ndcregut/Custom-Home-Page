fcfab70dcf9e4cdf559a39bfcf942575192a41b8
ndcregut <nathancregut@outlook.com>
2016-02-15 23:15:46 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#wrapper').fadeIn(2000);
+
+	// Slowly show sub-menu when hovering over main menu options
+	$('ul#dropdown > li').on('mouseenter', function() {
+		$(this).find('ul.sub-menu').slideDown('slow').show();
+		$(this).find('ul.sub-menu').css('display', 'inline-block');
+	});
+
+	// Slowly hide sub-menu when no longer hovering over the
+	// main menu option or its sub-menu
+	$('ul#dropdown > li').on('mouseleave', function() {
+		$(this).find('ul.sub-menu').slideUp('slow');
+	});
+});
