dfc6de582f1579e69124a5eff157fe40020f77c6
Nathan Cregut <mcr6114@live.com>
2016-08-28 16:45:27 -0500

    Changed the menus to open when clicked on rather than when hovered over

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	$('#wrapper').fadeIn(1000);
 
 	// Slowly show sub-menu when hovering over main menu options
-	$('ul#dropdown > li').on('mouseenter', function() {
-		$(this).find('ul.sub-menu').slideDown().show();
+	$('ul#dropdown > li').on('click', function() {
+		$(this).find('ul.sub-menu').slideDown(250).show();
 		$(this).find('ul.sub-menu').css('display', 'inline-block');
 	});
 
 	// Slowly hide sub-menu when no longer hovering over the
 	// main menu option or its sub-menu
 	$('ul#dropdown > li').on('mouseleave', function() {
-		$(this).find('ul.sub-menu').slideUp();
+		$(this).find('ul.sub-menu').slideUp(250);
 	});
 });
